263c85d1127b5da43206407a7381e9c091719248
Kostya-X <constantine-x@outlook.com>
2022-01-26 21:41:51 +0200

    display posts

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import ArticleView, HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleView.as_view(), name="article"),
 ]
--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,7 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+
+
+class ArticleView(DetailView):
+    model = Post
+    template_name = 'article.html'
